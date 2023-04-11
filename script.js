4a150b7d140c2908903592ec6ffd44e43f5797da
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-11 22:43:49 +0530

    added not show

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 const buttons = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-modal');
 
 const show = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+};
+
+const notShow = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 for (const button of buttons) {
@@ -15,5 +21,6 @@ for (const button of buttons) {
   });
 }
 
-
-
+close.addEventListener('click', function () {
+  notShow();
+});
