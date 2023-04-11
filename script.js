45d4f6ffbee51115f6c9cfa8842d1740d28999b1
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-11 22:42:01 +0530

    modal show implemented

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 'use strict';
 
-const modal = document.querySelectorAll('.show-modal');
+const buttons = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const show = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (const button of buttons) {
+  button.addEventListener('click', function () {
+    show();
+  });
+}
 
 
-modal.addEventListener('click', function(){
 
-})
