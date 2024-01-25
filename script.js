7622625c610888eed3a0f1fd0f685013dd0b3456
Dhruv Arora <dhruvarora.code@gmail.com>
2024-01-25 23:12:32 +0530

    implemented modal window

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 'use strict';
 
-const buttons = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const close = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const show = function () {
+const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const notShow = function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (const button of buttons) {
-  button.addEventListener('click', function () {
-    show();
-  });
-}
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-close.addEventListener('click', function () {
-  notShow();
-});
-
-overlay.addEventListener('click', function () {
-  notShow();
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    notShow();
+    closeModal();
   }
 });
